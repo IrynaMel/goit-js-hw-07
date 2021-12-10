379597fdf7f0bd780d0d60d1934bff65c46b7843
IrynaMel <iryna.melnyk270692@gmail.com>
2021-12-10 19:13:38 +0200

    homework7

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -3,28 +3,19 @@ import { galleryItems } from './gallery-items.js';
 const div = document.querySelector('.gallery');
 
 const images = galleryItems.map(({preview, original, description}) => 
-    `<a class="gallery__item" href=${original}>
+    `<li><a class="gallery__item" href=${original}>
   <img class="gallery__image" src=${preview} alt='${description}' />
-</a>
+</a></li>
 `).join("");
-    div.insertAdjacentHTML('beforeEnd', images);
+   
+div.insertAdjacentHTML('beforeEnd', images);
 
 
-
-    div.addEventListener("click", modalOpen)
-
-     new SimpleLightbox('.gallery a', {
-        captionSelector:'img',
-        captionType: 'alt',
-        captionsData: 'alt',
-        captionDelay: 250,
-        captionPosition:'bottom',
-    })
-
-    function modalOpen(e){
-        e.preventDefault();
-        
-        if (e.target === e.currentTarget) {
-            return }
- 
-        }
+     
+new SimpleLightbox('.gallery a', {
+    captionSelector:'img',
+    captionType: 'alt',
+    captionsData: 'alt',
+    captionDelay: 250,
+    captionPosition:'bottom',
+ })
